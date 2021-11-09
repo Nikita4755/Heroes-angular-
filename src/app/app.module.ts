0945a3172164ee694980bfe3ae745205235f58b3
NBorutenko <nikborutenko4755@gmail.com>
2021-11-09 12:30:48 +0300

    sddsa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,20 +9,33 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { MessagesComponent } from './messages/messages.component';
 
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatButtonModule} from '@angular/material/button';
+import { ButtonsComponent } from './buttons/buttons.component';
+
+
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatButtonModule,
+    ButtonsComponent
   ],
   declarations: [
     AppComponent,
     DashboardComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    ButtonsComponent
   ],
   bootstrap: [ AppComponent ]
 })
+
+
 export class AppModule { }
