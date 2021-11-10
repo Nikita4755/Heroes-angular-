53b86aa242787fdb69ca34557ff387515130e1d8
NBorutenko <nikborutenko4755@gmail.com>
2021-11-10 11:55:31 +0300

    123

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import { MessagesComponent } from './messages/messages.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import {MatButtonModule} from '@angular/material/button';
-import { ButtonsComponent } from './buttons/buttons.component';
+
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatTableModule} from '@angular/material/table';
+
 
 
 
@@ -24,15 +28,16 @@ import { ButtonsComponent } from './buttons/buttons.component';
     BrowserAnimationsModule,
     MatSliderModule,
     MatButtonModule,
-    ButtonsComponent
+    MatDividerModule,
+    MatIconModule,
+    MatInputModule,
+    MatTableModule
   ],
   declarations: [
     AppComponent,
     DashboardComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent,
-    ButtonsComponent
   ],
   bootstrap: [ AppComponent ]
 })
