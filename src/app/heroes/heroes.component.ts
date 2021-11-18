933747ba2cf8aca33fc34dd977a93cbc0aca0df5
NBorutenko <nikborutenko4755@gmail.com>
2021-11-18 21:14:20 +0300

    345

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -3,13 +3,26 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
+class Element {
+  position: number;
+  name: string;
+  strength: number;
+}
+
+const ELEMENT_HEROES: Element[]=[
+  {position: 1, name: 'dc', strength: 4},
+  {position: 2, name: 'dc', strength: 4},
+  {position: 3, name: 'dc', strength: 4},
+];
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes: Hero[] = [];
+  displavedColumns: string[] = ['position','name','strength'];
+  dataSource= ELEMENT_HEROES;
 
   constructor(private heroService: HeroService) { }
 
